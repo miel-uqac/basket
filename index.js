7417d2160448e8bb278c58b18268b4d5ad4bb657
Daimbow <julien-jamme@outlook.com>
2024-05-15 18:21:07 -0400

    ajustement email

--- index.js
+++ index.js
@@ -20,82 +20,83 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-const loginEmailPassword = async () => {
-  const loginEmail = document.querySelector("#txtEmail").value;
-  const loginPassword = document.querySelector("#txtPassword").value;
+document.addEventListener('DOMContentLoaded', () => {
+  const loginEmailPassword = async () => {
+    const loginEmail = document.querySelector("#txtEmail").value;
+    const loginPassword = document.querySelector("#txtPassword").value;
 
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-    const user = userCredential.user;
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+      const user = userCredential.user;
 
-    if (user.emailVerified) {
-      console.log("User is logged in and email is verified");
-      showApp();
-      showLoginState(user);
-    } else {
-      console.log("Email is not verified");
-      await signOut(auth); 
-      alert("Please verify your email before logging in.");
-      await sendEmailVerification(user);
-    }
-  } catch (error) {
-    console.log(error);
-    showLoginError(error);
-  }
-};
-
-const createAccount = async () => {
-  const loginEmail = document.querySelector("#txtEmail").value;
-  const loginPassword = document.querySelector("#txtPassword").value;
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, loginEmail, loginPassword);
-    console.log(userCredential.user);
-    await sendEmailVerification(auth.currentUser);
-    console.log("Email de vérification envoyé !");
-  } catch (error) {
-    console.log(error);
-    showLoginError(error);
-  }
-};
-
-const monitorAuthState = async () => {
-  onAuthStateChanged(auth, user => {
-    if (user) {
       if (user.emailVerified) {
+        console.log("User is logged in and email is verified");
         showApp();
         showLoginState(user);
-        hideLoginError();
       } else {
-        signOut(auth);
+        console.log("Email is not verified");
+        await signOut(auth); 
+        alert("Please verify your email before logging in.");
+        await sendEmailVerification(user);
+      }
+    } catch (error) {
+      console.log(error);
+      showLoginError(error);
+    }
+  };
+
+  const createAccount = async () => {
+    const loginEmail = document.querySelector("#txtEmail").value;
+    const loginPassword = document.querySelector("#txtPassword").value;
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, loginEmail, loginPassword);
+      console.log(userCredential.user);
+      await sendEmailVerification(auth.currentUser);
+      console.log("Email de vérification envoyé !");
+    } catch (error) {
+      console.log(error);
+      showLoginError(error);
+    }
+  };
+
+  const monitorAuthState = async () => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        if (user.emailVerified) {
+          showApp();
+          showLoginState(user);
+          hideLoginError();
+        } else {
+          signOut(auth);
+          showLoginForm();
+          const lblAuthState = document.querySelector("#lblAuthState");
+          lblAuthState.textContent = "Please verify your email before logging in.";
+        }
+      } else {
         showLoginForm();
         const lblAuthState = document.querySelector("#lblAuthState");
-        lblAuthState.textContent = "Please verify your email before logging in.";
+        lblAuthState.textContent = "You are not logged in";
       }
-    } else {
-      showLoginForm();
-      const lblAuthState = document.querySelector("#lblAuthState");
-      lblAuthState.textContent = "You are not logged in";
-    }
-  });
-};
+    });
+  };
 
-const btnLogin = document.querySelector("#btnLogin");
-btnLogin.addEventListener("click", loginEmailPassword);
+  const btnLogin = document.querySelector("#btnLogin");
+  btnLogin.addEventListener("click", loginEmailPassword);
 
-const buttonCreate = document.querySelector("#buttonCreateAccount");
-buttonCreate.addEventListener("click", createAccount);
+  const buttonCreate = document.querySelector("#buttonCreateAccount");
+  buttonCreate.addEventListener("click", createAccount);
 
-if (location.hostname === "127.0.0.1") {
-  connectAuthEmulator(auth, "http://127.0.0.1:5002");
-  connectDatabaseEmulator(db, "127.0.0.1", 8000);
-}
+  if (location.hostname === "127.0.0.1") {
+    connectAuthEmulator(auth, "http://127.0.0.1:5002");
+    connectDatabaseEmulator(db, "127.0.0.1", 8000);
+  }
 
-monitorAuthState();
+  monitorAuthState();
 
-const logout = async () => {
-  await signOut(auth);
-  //test
-};
+  const logout = async () => {
+    await signOut(auth);
+  };
 
-const btnLogout = document.querySelector("#btnLogout");
-btnLogout.addEventListener("click", logout);
+  const btnLogout = document.querySelector("#btnLogout");
+  btnLogout.addEventListener("click", logout);
+});
