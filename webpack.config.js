3e3e3083024ed15c5c1c8a5251b0325cc9367d9c
Daimbow <julien-jamme@outlook.com>
2024-05-19 11:09:58 -0400

    ajout css,image et style pour formulaire de connexion

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,11 @@ module.exports = (env) => {
         mode: isProduction ? 'production' : 'development',
         devtool: isProduction ? 'hidden-source-map' : 'eval-source-map',
         entry: './javascript/index.js',
+        watch:isProduction ? false : true,
+        watchOptions: {
+            aggregateTimeout: 200,
+            ignored: ['**/*', '!**/public/**', '**/node_modules'],
+          },
         output: {
             path: path.resolve(__dirname, 'public/build'),
             filename: 'bundle.js'
