abb5f65d1ce1396b5ec589af3313f2b711a6c017
Escartem <escartem.yt@gmail.com>
2026-03-28 14:52:21 -0400

    game page

---
+++ app/game/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Button, LoadingBox, UqacBox, Wrapper } from "@/components/uqac-utils";
+import { useAuth } from "@/hooks/useAuth";
+import { getClient } from "@/lib/uqac-lib";
+import { ArrowBigLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function GamePage() {
+    const client = getClient()
+    const router = useRouter();
+    const user = useAuth();
+    const [token, setToken] = useState("")
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const gameToken = params.get("token") || "";
+
+        if (!gameToken) {
+            router.push("/qr");
+        } else {
+            setToken(gameToken);
+        }
+    }, []);
+    
+    return (
+        <>
+        <Wrapper className="flex-col">
+            {user ? (
+                <>
+                <UqacBox className="w-[95%] h-[95%] text-black text-[2.5rem] leading-[3.5rem]" title={"Game"}>
+                    {true ? (
+                        <>
+                        <div>
+                            <span className="font-bold">Goals : </span>
+                            <span>...</span>
+                        </div>
+                        <div>
+                            <span className="font-bold">Accuracy : </span>
+                            <span>...</span>
+                        </div>
+                        <div>
+                            <span className="font-bold">Best streak : </span>
+                            <span>...%</span>
+                        </div>
+                        <div>
+                            <span className="font-bold">Most launched item : </span>
+                            <span>...</span>
+                        </div>
+                        </>
+                    ) : (
+                        <LoadingBox className="w-full h-full" />
+                    )}
+                </UqacBox>
+                
+                <div className="flex mt-4 gap-4">
+                    <Button className="flex items-center justify-center border-2 border-white/40 font-bold" onClick={(e: any) => {router.push("/leaderboard")}}>
+                        <ArrowBigLeft className="mr-1" />
+                        Account
+                    </Button>
+                </div>
+                </>
+            ) : (
+                <LoadingBox />
+            )}
+        </Wrapper>
+        </>
+    )
+}
